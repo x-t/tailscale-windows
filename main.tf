ad45d7c84363f02e8e832477288b8f4e01b23812
zxyz <xdasht@pm.me>
2023-09-09 21:19:46 +0300

    firewall

--- main.tf
+++ main.tf
@@ -44,6 +44,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Tailscale"
+    priority                   = 900
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "41641"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
